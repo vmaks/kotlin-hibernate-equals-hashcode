138f207b8fa8bb73df2547f702379aac288e5b30
Maksim Vlasov <vlasov.maksim@hotmail.com>
2019-07-21 16:29:36 +0300

    Add basic project from start.spring.io

---
+++ src/main/kotlin/com/maks/kotlin_hibernate_equals_hashcode/KotlinHibernateEqualsHashcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.maks.kotlin_hibernate_equals_hashcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinHibernateEqualsHashcodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinHibernateEqualsHashcodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/maks/kotlin_hibernate_equals_hashcode/KotlinHibernateEqualsHashcodeApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.maks.kotlin_hibernate_equals_hashcode
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinHibernateEqualsHashcodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
