562d88b472fec3a178852150a8116c0cf683c1e6
Maksim Vlasov <vlasov.maksim@hotmail.com>
2019-10-06 18:54:09 +0300

    Make an example

--- src/main/kotlin/com/maks/kotlin_hibernate_equals_hashcode/KotlinHibernateEqualsHashcodeApplication.kt
+++ src/main/kotlin/com/maks/kotlin_hibernate_equals_hashcode/KotlinHibernateEqualsHashcodeApplication.kt
@@ -1,11 +1,25 @@
 package com.maks.kotlin_hibernate_equals_hashcode
 
+import com.maks.kotlin_hibernate_equals_hashcode.model.EntityWithBusinessKey
+import com.maks.kotlin_hibernate_equals_hashcode.repository.EntityWithBusinessKeyRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.time.LocalDate
 
 @SpringBootApplication
-class KotlinHibernateEqualsHashcodeApplication
+class KotlinHibernateEqualsHashcodeApplication(val entityWithBusinessKeyRepository: EntityWithBusinessKeyRepository) :
+    CommandLineRunner {
+    override fun run(vararg args: String?) {
+        val entity = EntityWithBusinessKey(
+            date = LocalDate.now(),
+            message = "Hello",
+            businessKey = "12345"
+        )
+        println(entityWithBusinessKeyRepository.save(entity))
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<KotlinHibernateEqualsHashcodeApplication>(*args)
+    runApplication<KotlinHibernateEqualsHashcodeApplication>(*args)
 }
